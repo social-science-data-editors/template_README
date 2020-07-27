717978e725eea5cfcaeeee7df6868895c67dcef0
Lars Vilhuber <lars.vilhuber@cornell.edu>
2020-07-27 12:34:36 -0400

    Addressing the file generation issues

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,3 +1,5 @@
-docker run  --rm  --workdir /github/workspace  -v "$(pwd)":"/github/workspace" pandoc/latex:2.9 _README.md --standalone -t html5                          -o templates/README.html --metadata pagetitle=README --lua-filter=_scripts/links-to-html.lua
-wkhtmltopdf templates/README.html templates/README.pdf
+_scripts/set_webroot.sh template-README.md _README.md
+chmod a+r _README.md
+docker run  --rm  --workdir /github/workspace  -v "$(pwd)":"/github/workspace" pandoc/latex:2.9 _README.md --standalone -t html5 -f gfm                         -o templates/README.html --metadata pagetitle=README --lua-filter=_scripts/links-to-html.lua
+env QT_STYLE_OVERRIDE=fusion wkhtmltopdf templates/README.html templates/README.pdf
 docker run  --rm  --workdir /github/workspace  -v "$(pwd)":"/github/workspace" pandoc/latex:2.9 _README.md --standalone -t docx                           -o templates/README.docx --lua-filter=_scripts/links-to-html.lua
