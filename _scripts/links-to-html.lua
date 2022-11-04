be16fbfcb029575b34a5114773cbc090863db91f
larsvilhuber <larsvilhuber@users.noreply.github.com>
2022-11-04 22:11:30 +0000

    deploy: f0ae68749b596a55e28f69cdee1c22057c45063e

--- _scripts/links-to-html.lua
+++
@@ -1,6 +0,0 @@
--- links-to-html.lua
--- from https://stackoverflow.com/questions/40993488/convert-markdown-links-to-html-with-pandoc
-function Link(el)
-  el.target = string.gsub(el.target, "%.md", ".html")
-  return el
-end
--- _scripts/set_webroot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-WEBROOT=https://social-science-data-editors.github.io/template_README/
-sed 's@\](\([a-gi-zA-Z]\)@]('"$WEBROOT"'\1@g' $1 > $2
